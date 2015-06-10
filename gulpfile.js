88b99a1a6d38228a6e92e5e8ffb18384bb670cbd
zhaohao.yzh <zhaohao.yzh@alibaba-inc.com>
2015-06-10 17:29:22 +0800

    add build shell

---
+++ gulpfile.js
@@ -0,0 +1,25 @@
+var gulp = require('gulp');
+var code = require('gulp-code');
+var connect = require('gulp-connect');
+var rename = require('gulp-rename');
+var open = require('gulp-open');
+
+gulp.task('js', function () {
+  return gulp.src('./yscroll.js')
+    .pipe(code.lint())
+    .pipe(rename(function (path) {
+      path.basename = 'yscroll.min';
+    }))
+    .pipe(code.minify())
+    .pipe(gulp.dest('./'));
+});
+
+gulp.task('connect', function () {
+  return connect.server({
+    root: ['./'],
+    livereload: true,
+    port: '3000'
+  });
+});
+
+gulp.task('default', ['js']);
