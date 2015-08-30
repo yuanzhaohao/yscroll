f9965c1bd4e1f98929461272fb6740648b19e3dc
yuanzhaohao <yuanzhaohao@163.com>
2015-08-30 15:14:11 +0800

    增加测试用例

--- test/tests.js
+++ test/tests.js
@@ -9,6 +9,39 @@ describe('YScroll', function () {
   var $yscroll, y;
   beforeEach(function () {
     $yscroll = $(html).appendTo('#test');
+    console.log($yscroll)
     y = new YScroll($yscroll[0]);
+  });
+  afterEach(function() {
+    $(y.scroller).remove();
+  });
+
+  describe('constructor', function() {
+    context('when set dom element', function() {
+      it('element should be set', function() {
+        var div = document.createElement('div');
+        var f = YScroll(div);
+        expect(f.scroller).to.be(div);
+      });
+    });
+
+    context('when set string(css seclector)', function() {
+      context('when exist element', function() {
+        it('element should be search by selector', function() {
+          var $foo = $('<div id="foo">').appendTo('body');
+          var f = YScroll('#foo');
+          expect(f.scroller).to.be($foo.get(0));
+          $foo.remove();
+        });
+      });
+
+      context('when not exist element', function() {
+        it('should throw error' ,function() {
+          expect(function() {
+            Yscroll('#foo');
+          }).to.throwError(/element not found/);
+        });
+      });
+    });
   });
 });
